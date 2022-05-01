d0be1082bbb9c86a6691d245e2085990fccf5b8e
Ava-creates <avanitiwari2002@gmail.com>
2022-05-01 19:13:26 +0400

    dbkasfb

--- script2.js
+++ script2.js
@@ -1,3 +1,7 @@
 document.getElementById("previous").onclick = function () {
     location.href = "index.html";
 }
+
+document.getElementById("next").onclick = function () {
+    location.href = "play.html";
+}
