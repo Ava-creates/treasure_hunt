b62583b4852132fef369ad8a6023c2343cdc5c0a
Alima Zhagufarova <az2180@nyu.edu>
2022-05-04 03:14:10 +0400

    riddle 1 ready with the validation

--- script_riddles.js
+++ script_riddles.js
@@ -1,11 +1,44 @@
-var message = document.getElementById("message");
+const submit = document.querySelector("button");
+submit.addEventListener("click", function() {
+  const answer = document.getElementById("answer");
+    let valid = true;
 
-submitButton.addEventListener("click", function myFunction() {
-  // Get the value of the input field with id="numb"
-  let x = document.getElementById("answer").value;
-  // If x is Not a Number or less than one or greater than 10
-  let text;
-  if (isNaN(x) || x.length < 5 || x.length >10 ) {
-    message.textContent += "This just got added";
-  }
-});
+    if (!answer.value) {
+      console.log("ouch");
+      const emptyAnswer = document.getElementById("emptyAnswer");
+      emptyAnswer.classList.add("visible");
+      answer.classList.add("invalid");
+      emptyAnswer.setAttribute("aria-hidden", false);
+      emptyAnswer.setAttribute("aria-invalid", true);
+
+      correctAnswer.classList.remove("visible");
+      incorrectAnswer.classList.remove("visible");
+    }
+    else if (answer.value!="leaves") {
+      const incorrectAnswer = document.getElementById("incorrectAnswer");
+      incorrectAnswer.classList.add("visible");
+      answer.classList.add("invalid");
+      incorrectAnswer.setAttribute("aria-hidden", false);
+      incorrectAnswer.setAttribute("aria-invalid", true);
+
+      correctAnswer.classList.remove("visible");
+      emptyAnswer.classList.remove("visible");
+    }
+    else{
+      const correctAnswer = document.getElementById("correctAnswer");
+      correctAnswer.classList.add("visible");
+      answer.classList.add("valid");
+      correctAnswer.setAttribute("aria-hidden", false);
+      correctAnswer.setAttribute("aria-valid", true);
+
+      emptyAnswer.classList.remove("visible");
+      incorrectAnswer.classList.remove("visible");
+
+      submit.style.display = "none";
+      
+      let btn = document.createElement("button");
+      btn.innerHTML = "NEXT";
+      submitButton.appendChild(btn);
+      btn.classList.add('submitButton');
+    }
+  });
