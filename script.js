6d16c918843ba1f3e654113fda77560a0bf3f330
Nicholas Raffone <nicholas.raffone@gmail.com>
2022-05-04 16:20:34 +0400

    checking shit

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ const textelem = document.getElementById('pressme')
 const fulltext = 'press me!'
 const cover = document.getElementById('cover')
 window.onload = ()=>{
-    for(let i = 0; i < (fulltext.length+1); i++){
-        setTimeout(()=>{
-            textelem.innerText = fulltext.slice(0,i)
-            if(fulltext[i-1]===' '){
-                textelem.style.paddingRight='0.5em'
-            }else{
-                textelem.style.paddingRight='0em'
-            }
-        },i*500)
+    if(textelem){
+        for(let i = 0; i < (fulltext.length+1); i++){
+            setTimeout(()=>{
+                textelem.innerText = fulltext.slice(0,i)
+                if(fulltext[i-1]===' '){
+                    textelem.style.paddingRight='0.5em'
+                }else{
+                    textelem.style.paddingRight='0em'
+                }
+            },i*500)
+        }
     }
     cover.style.visibility='visible';
     cover.style.opacity=0;
