c9149dfcd29c1c80a7796bb14e2a2bfbc2b22081
Nicholas Raffone <nicholas.raffone@gmail.com>
2022-05-04 14:10:43 +0400

    fixing answer page form handling:

--- script_riddles.js
+++ script_riddles.js
@@ -1,44 +1,69 @@
 const submit = document.querySelector("button");
-submit.addEventListener("click", function() {
+
+const form = document.getElementById('mainform')
+
+const textelem = document.getElementById('youranswer')
+const fulltext = 'Your Answer:'
+window.onload = ()=>{
+    for(let i = 0; i < (fulltext.length+1); i++){
+        setTimeout(()=>{
+            textelem.innerText = fulltext.slice(0,i)
+            if(fulltext[i-1]===' '){
+                textelem.style.paddingRight='0.5em'
+            }else{
+                textelem.style.paddingRight='0em'
+            }
+        },i*350)
+    }
+}
+
+const answerQuestion = () =>{
   const answer = document.getElementById("answer");
-    let valid = true;
+  let valid = true;
 
-    if (!answer.value) {
-      console.log("ouch");
-      const emptyAnswer = document.getElementById("emptyAnswer");
-      emptyAnswer.classList.add("visible");
-      answer.classList.add("invalid");
-      emptyAnswer.setAttribute("aria-hidden", false);
-      emptyAnswer.setAttribute("aria-invalid", true);
+  if (!answer.value) {
+    console.log("ouch");
+    const emptyAnswer = document.getElementById("emptyAnswer");
+    emptyAnswer.classList.add("visible");
+    answer.classList.add("invalid");
+    emptyAnswer.setAttribute("aria-hidden", false);
+    emptyAnswer.setAttribute("aria-invalid", true);
 
-      correctAnswer.classList.remove("visible");
-      incorrectAnswer.classList.remove("visible");
-    }
-    else if (answer.value!="leaves") {
-      const incorrectAnswer = document.getElementById("incorrectAnswer");
-      incorrectAnswer.classList.add("visible");
-      answer.classList.add("invalid");
-      incorrectAnswer.setAttribute("aria-hidden", false);
-      incorrectAnswer.setAttribute("aria-invalid", true);
+    correctAnswer.classList.remove("visible");
+    incorrectAnswer.classList.remove("visible");
+  }
+  else if (answer.value!="leaves") {
+    const incorrectAnswer = document.getElementById("incorrectAnswer");
+    incorrectAnswer.classList.add("visible");
+    answer.classList.add("invalid");
+    incorrectAnswer.setAttribute("aria-hidden", false);
+    incorrectAnswer.setAttribute("aria-invalid", true);
 
-      correctAnswer.classList.remove("visible");
-      emptyAnswer.classList.remove("visible");
-    }
-    else{
-      const correctAnswer = document.getElementById("correctAnswer");
-      correctAnswer.classList.add("visible");
-      answer.classList.add("valid");
-      correctAnswer.setAttribute("aria-hidden", false);
-      correctAnswer.setAttribute("aria-valid", true);
+    correctAnswer.classList.remove("visible");
+    emptyAnswer.classList.remove("visible");
+  }
+  else{
+    const correctAnswer = document.getElementById("correctAnswer");
+    correctAnswer.classList.add("visible");
+    answer.classList.add("valid");
+    correctAnswer.setAttribute("aria-hidden", false);
+    correctAnswer.setAttribute("aria-valid", true);
 
-      emptyAnswer.classList.remove("visible");
-      incorrectAnswer.classList.remove("visible");
+    emptyAnswer.classList.remove("visible");
+    incorrectAnswer.classList.remove("visible");
 
-      submit.style.display = "none";
-      
-      let btn = document.createElement("button");
-      btn.innerHTML = "NEXT";
-      submitButton.appendChild(btn);
-      btn.classList.add('submitButton');
-    }
-  });
+    submit.style.display = "none";
+    
+    let btn = document.createElement("button");
+    btn.innerHTML = "NEXT";
+    submitButton.appendChild(btn);
+    btn.classList.add('submitButton');
+  }
+}
+
+form.addEventListener('submit',(e)=>{
+  e.preventDefault();
+  answerQuestion();
+})
+
+submit.addEventListener("click", answerQuestion);
