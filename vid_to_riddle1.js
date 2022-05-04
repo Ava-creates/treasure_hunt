d8ccee30b7a57afad9d9409b0bfb70aad066122f
Nicholas Raffone <nicholas.raffone@gmail.com>
2022-05-04 14:24:54 +0400

    add map

--- vid_to_riddle1.js
+++ vid_to_riddle1.js
@@ -19,3 +19,12 @@ document.getElementById("play").onclick = function () {
    
    
 }
+
+const map = document.getElementById('map')
+map.addEventListener('click', ()=>{
+  if(!map.classList.contains('enlarge')){
+    map.classList.add('enlarge')
+  }else{
+    map.classList.remove('enlarge')
+  }
+})
