459a998227ad650c61958b177b07f799044c643b
Alima Zhagufarova <az2180@nyu.edu>
2022-05-05 17:48:19 +0400

    maps!!

--- mapscript.js
+++ mapscript.js
@@ -1,25 +1,12 @@
-const map = document.getElementById('map')
-
 const lfoot = document.getElementById('leftfoot')
 const rfoot = document.getElementById('rightfoot')
 
-map.addEventListener('click',()=>{
-    if(!map.classList.contains('enlarge')){
-        map.classList.add('enlarge')
-        rfoot.style.animation = 'move-right 10s linear infinite';
-        lfoot.style.animation = 'move-left 10s linear infinite';
-        setTimeout(()=>{
-            lfoot.style.display = 'block'
-            rfoot.style.display = 'block'
-        },500)
-    }else{
-        map.classList.remove('enlarge')
-        rfoot.style.animation = '';
-        lfoot.style.animation = '';
-        lfoot.style.display = 'none'
-        rfoot.style.display = 'none'
-    }
-})
+function showImage() {
+      document.getElementById("final").style.visibility = "visible";
+  }
+setTimeout("showImage()", 5000);
+
+
 let dynamicStyles = null;
 
 function addAnimation(body) {
@@ -32,26 +19,27 @@ function addAnimation(body) {
   dynamicStyles.sheet.insertRule(body, dynamicStyles.length);
 }
 
-const [minVW, maxVW] = [15, 60]
-const [minVH, maxVH] = [20, 35]
-const maxrange = 5;
+const [startX, startY] = [45,30]
+const rotation = 180;
+const [dx, dy] = [0, 7]
+const [lrdx, lrdy] = [2, 3]
 
-const NUM_STEPS = 5;
+const NUM_STEPS = 4;
 const STEPLEN = 100/((NUM_STEPS - 1) * 3 + 1); // 0%, ..., 100% where ... is 3 keyframes per step in NUM_STEPS - 1
 
 const steplist = []
 
-steplist.push([Math.random()*(maxVW-minVW)+minVW, Math.random()*(maxVH-minVH)+minVH])
+steplist.push([startX, startY])
 
 for(let i = 1; i<NUM_STEPS - 1; i++){
-    steplist.push([steplist[i-1][0]+maxrange, steplist[i-1][1]-maxrange])
+    steplist.push([steplist[i-1][0]+dx, steplist[i-1][1]+dy])
 }
 steplist.push(steplist[0])
 
-const altsteplist = steplist.map((step,index)=>index%2===0?[step[0]-3, step[1]+8]:[step[0]+5, step[1]-5])
+const altsteplist = steplist.map((step,index)=>[step[0]+lrdx, step[1]+lrdy])
 
 const makeFrames = (steplist, name)=>{
-   const first = 
+   const first =
     `
     0% {
         opacity: 1;
@@ -76,20 +64,20 @@ const makeFrames = (steplist, name)=>{
         ${STEPLEN*(3*i+3)}% {
             opacity: 1;
             left: ${steplist[i+1][0]}vw;
-            top: ${steplist[i+1][1]}vh; 
+            top: ${steplist[i+1][1]}vh;
         }
         `)
     }
-    const last = 
+    const last =
         `
         100% {
             opacity: 1;
             left: ${steplist[steplist.length-1][0]}vw;
             top: ${steplist[steplist.length-1][1]}vh;
         }
-        ` 
+        `
     return( `
-    @keyframes ${name} { 
+    @keyframes ${name} {
         ${first}
         ${stepframes.join('\n')}
         ${last}
@@ -97,7 +85,9 @@ const makeFrames = (steplist, name)=>{
    `)
 }
 
-
+console.log(makeFrames(steplist, 'move-left'))
 
 addAnimation(makeFrames(steplist, 'move-left'));
 addAnimation(makeFrames(altsteplist, 'move-right'));
+rfoot.style.animation = 'move-right 10s linear infinite';
+lfoot.style.animation = 'move-left 10s linear infinite';
