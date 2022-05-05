3db29af89b30553d621b5b9f3f0097545a635a18
Nicholas Raffone <nicholas.raffone@gmail.com>
2022-05-05 12:20:04 +0400

    footsteps

--- mapscript.js
+++ mapscript.js
@@ -1,5 +1,8 @@
 const map = document.getElementById('map')
 
+const lfoot = document.getElementById('leftfoot')
+const rfoot = document.getElementById('rightfoot')
+
 map.addEventListener('click',()=>{
     if(!map.classList.contains('enlarge')){
         map.classList.add('enlarge')
@@ -7,27 +10,90 @@ map.addEventListener('click',()=>{
         map.classList.remove('enlarge')
     }
 })
+let dynamicStyles = null;
+
+function addAnimation(body) {
+  if (!dynamicStyles) {
+    dynamicStyles = document.createElement('style');
+    dynamicStyles.type = 'text/css';
+    document.head.appendChild(dynamicStyles);
+  }
+
+  dynamicStyles.sheet.insertRule(body, dynamicStyles.length);
+}
+
+const [minVW, maxVW] = [15, 60]
+const [minVH, maxVH] = [20, 35]
+const maxrange = 5;
+
+const NUM_STEPS = 5;
+const STEPLEN = 100/((NUM_STEPS - 1) * 3 + 1); // 0%, ..., 100% where ... is 3 keyframes per step in NUM_STEPS - 1
+
+const steplist = []
+
+steplist.push([Math.random()*(maxVW-minVW)+minVW, Math.random()*(maxVH-minVH)+minVH])
+
+for(let i = 1; i<NUM_STEPS - 1; i++){
+    steplist.push([steplist[i-1][0]+maxrange, steplist[i-1][1]-maxrange])
+}
+steplist.push(steplist[0])
+
+const altsteplist = steplist.map((step,index)=>index%2===0?[step[0]-3, step[1]+8]:[step[0]+5, step[1]-5])
+
+const makeFrames = (steplist, name)=>{
+   const first = 
+    `
+    0% {
+        opacity: 1;
+        left: ${steplist[0][0]}vw;
+        top: ${steplist[0][1]}vh;
+      }
+    `
+
+    const stepframes = [];
+    for(let i =0; i< steplist.length-1; i++){
+        stepframes.push(`
+        ${STEPLEN*(3*i+1)}% {
+            opacity: 0;
+            left: ${steplist[i][0]}vw;
+            top: ${steplist[i][1]}vh;
+        }
+        ${STEPLEN*(3*i+2)}% {
+            opacity: 0;
+            left: ${steplist[i+1][0]}vw;
+            top: ${steplist[i+1][1]}vh;
+        }
+        ${STEPLEN*(3*i+3)}% {
+            opacity: 1;
+            left: ${steplist[i+1][0]}vw;
+            top: ${steplist[i+1][1]}vh; 
+        }
+        `)
+    }
+    const last = 
+        `
+        100% {
+            opacity: 1;
+            left: ${steplist[steplist.length-1][0]}vw;
+            top: ${steplist[steplist.length-1][1]}vh;
+        }
+        ` 
+    return( `
+    @keyframes ${name} { 
+        ${first}
+        ${stepframes.join('\n')}
+        ${last}
+     }
+   `)
+}
 
 
-var $rightfoot = $("#rightfoot"),
-    $leftfoot = $("#leftfoot");
 
-var tl = new TimelineMax({repeat:-1})
+addAnimation(makeFrames(steplist, 'move-left'));
 
-  
+lfoot.style.animation = 'move-left 10s linear infinite';
 
-  var ease = SteppedEase.config(5);
 
-tl
+addAnimation(makeFrames(altsteplist, 'move-right'));
 
-  .to($rightfoot, 0.25, {autoAlpha:1,},1)
-  .to($leftfoot, 0.25, {autoAlpha:1,},1)
-  .to($leftfoot, 3, 
-
-  {bezier:{ curviness: .3, values:[{x:0, y:0},{x:13, y:10}, {x:19, y:40}, {x:35, y:54}, {x:45, y:83}, {x:50, y:90},{x:57, y:150,}],
-  autoRotate:90}, ease:ease},1)
-
-  .to($rightfoot, 3, 
-  {bezier:{ curviness: 1, values:[{x:0, y:0}, {x:16, y:10}, {x:30, y:40,}, {x:40, y:54}, {x:50, y:83}, {x:56, y:90,}, {x:36, y:150,}],
-  autoRotate:100}, ease:ease},1.25)
-
+rfoot.style.animation = 'move-right 10s linear infinite';
